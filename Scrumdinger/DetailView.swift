5468a23a6710027601e76dcc9f364215212ee69f
Joel Lacerda <joellacerdaol@gmail.com>
2023-07-27 08:23:26 -0300

    create the edit view

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -9,6 +9,7 @@ import SwiftUI
     
 struct DetailView: View {
     let scrum: DailyScrum
+    @State private var isPresentingEditView = false
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
@@ -41,6 +42,29 @@ struct DetailView: View {
             }
         }
         .navigationTitle(scrum.title)
+        .toolbar {
+            Button("Edit") {
+                isPresentingEditView = true
+            }
+        }
+        .sheet(isPresented: $isPresentingEditView) {
+            NavigationStack {
+                DetailEditView()
+                    .navigationTitle(scrum.title)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel") {
+                                isPresentingEditView = false
+                            }
+                        }
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Done") {
+                                isPresentingEditView = false
+                            }
+                        }
+                    }
+            }
+        }
     }
 }
     
