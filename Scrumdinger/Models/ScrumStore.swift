ce479edf363e7266871ddd8b835f3be75af6b229
Joel Lacerda <joellacerdaol@gmail.com>
2024-01-11 16:52:47 -0300

    Create a data store

---
+++ Scrumdinger/Models/ScrumStore.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumStore.swift
+//  Scrumdinger
+//
+//  Created by Joel Lacerda on 11/01/24.
+//
+
+import SwiftUI
+
+class ScrumStore: ObservableObject {
+    @Published var scrums: [DailyScrum] = []
+    
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            .appendingPathComponent("scrums.data")
+    }
+}
