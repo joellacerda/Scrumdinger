72d8e78cf41b3aa5c8e6f2f84320f7da4f5ad99f
Joel Lacerda <joellacerdaol@gmail.com>
2024-01-18 11:25:04 -0300

    Create the meeting timer view

---
+++ Scrumdinger/Views/MeetingTimerView.swift
@@ -0,0 +1,41 @@
+//
+//  MeetingTimerView.swift
+//  Scrumdinger
+//
+//  Created by Joel Lacerda on 18/01/24.
+//
+
+import SwiftUI
+
+struct MeetingTimerView: View {
+    let speakers: [ScrumTimer.Speaker]
+    let theme: Theme
+    
+    private var currentSpeaker: String {
+        speakers.first(where: { !$0.isCompleted })?.name ?? "Someone"
+    }
+    
+    var body: some View {
+        Circle()
+            .strokeBorder(lineWidth: 24)
+            .overlay {
+                VStack {
+                    Text(currentSpeaker)
+                        .font(.title)
+                    Text("is speaking")
+                }
+                .accessibilityElement(children: .combine)
+                .foregroundStyle(theme.accentColor)
+            }
+    }
+}
+
+struct MeetingTimerView_Previews: PreviewProvider {
+    static var speakers: [ScrumTimer.Speaker] {
+        [ScrumTimer.Speaker(name: "Bill", isCompleted: true), ScrumTimer.Speaker(name: "Cathy", isCompleted: false)]
+    }
+    
+    static var previews: some View {
+        MeetingTimerView(speakers: speakers, theme: .yellow)
+    }
+}
